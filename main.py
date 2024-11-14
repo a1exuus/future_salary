3e9168c4a6c9dda66458cba669adae7899c8ac9b
a1exuus <162327737+a1exuus@users.noreply.github.com>
2024-11-14 20:32:48 +0300

    fixed another packet of errors

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from itertools import count
 
 
 def calculate_salary(salary_from, salary_to):
-    if salary_from == 0 or not salary_from:
+    if not salary_from:
         middle_salary = salary_to * 0.8
-    elif salary_to == 0 or not salary_to:
+    elif not salary_to:
         middle_salary = salary_from * 1.2
     else:
         middle_salary = (salary_to + salary_from) / 2
@@ -23,16 +23,13 @@ def predict_salary_hh(salary):
 
 
 def predict_salary_sj(salary_from, salary_to):
-    if salary_from is not None and salary_to is not None:
+    if salary_from and salary_to:
         return calculate_salary(salary_from, salary_to)
     return 0
 
 
-
 def predict_rub_salary_hh(programming_languages):
     avg_vacancy_salary = {}
-    vacancies_found = 0
-    vacancies_processed = 0
     for programming_language in programming_languages:
         avg_vacancy_salary[programming_language] = {}
         url = 'https://api.hh.ru/vacancies'
@@ -40,31 +37,29 @@ def predict_rub_salary_hh(programming_languages):
         params = {
             'text': f'Программист {programming_language}',
             'area': 1
-                   }
+        }
         for page in count(0):
             sleep(1)
             response = requests.get(url, params=params)
             response.raise_for_status()
-            response_data = response.json()
+            api_response_content = response.json()
             params['page'] = page
-            for vacancy in response_data['items']:
-                vacancies_found += 1
+            for vacancy in api_response_content['items']:
                 middle_salary = predict_salary_hh(vacancy.get('salary'))
                 if middle_salary:
-                    vacancies_processed += 1
                     middle_salaries.append(middle_salary)
-            if page >= response_data['pages']:
+            if page >= api_response_content['pages']:
                 break
-        avg_vacancy_salary[programming_language]['average_salary'] = int(sum(middle_salaries) / len(middle_salaries))
-        avg_vacancy_salary[programming_language]['vacancies_found'] = vacancies_found
-        avg_vacancy_salary[programming_language]['vacancies_processed'] = vacancies_processed
+        avg_vacancy_salary[programming_language] = {
+                'average_salary': int(sum(middle_salaries) / len(middle_salaries)),
+                'vacancies_found': api_response_content['found'],
+                'vacancies_processed': len(middle_salaries)
+            }
     return avg_vacancy_salary
 
 
 def predict_rub_salary_sj(token, programming_languages):
     avg_vacancy_salary = {}
-    vacancies_found = 0
-    vacancies_processed = 0
     for programming_language in programming_languages:
         avg_vacancy_salary[programming_language] = {}
         url = 'https://api.superjob.ru/2.0/vacancies'
@@ -85,45 +80,47 @@ def predict_rub_salary_sj(token, programming_languages):
             if not vacancy:
                 continue
             middle_salary = int(predict_salary_sj(vacancy['payment_from'], vacancy['payment_to']))
-            if middle_salary != 0:
-                vacancies_processed += 1
+            if middle_salary:
                 middle_salaries.append(middle_salary)
-            vacancies_found += 1
         if middle_salaries:
-            avg_vacancy_salary[programming_language]['average_salary'] = int(sum(middle_salaries) / len(middle_salaries))
-            avg_vacancy_salary[programming_language]['vacancies_found'] = vacancies_found
-            avg_vacancy_salary[programming_language]['vacancies_processed'] = vacancies_processed
+            try: 
+                avg_salary = int(sum(middle_salaries) / len(middle_salaries))
+            except ZeroDivisionError:
+                avg_salary = None
+            avg_vacancy_salary[programming_language] = {
+                'average_salary': avg_salary,
+                'vacancies_found': response['total'],
+                'vacancies_processed': len(middle_salaries),
+            }
+        else:
+            avg_vacancy_salary[programming_language] = {
+                'vacancies_found': response['total']
+            }
     return avg_vacancy_salary
 
 
-def create_table_sj(processed_information):
+def create_table(processed_information, source):
     table_data = [
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
     ]
     information_keys = list(processed_information.keys())
     for key in information_keys:
-        data = [key, processed_information[key]['vacancies_found'], processed_information[key]['vacancies_processed'], processed_information[key]['average_salary']]
+        try:
+            data = [key, processed_information[key]['vacancies_found'], processed_information[key]['vacancies_processed'], processed_information[key]['average_salary']]
+        except:
+            data = [key, processed_information[key]['vacancies_found'], 0, 0]
         table_data.append(data)
     table = terminaltables.AsciiTable(table_data)
-    table.title = 'SuperJob Moscow'
+    if source == 'sj':
+        table.title = 'SuperJob Moscow'
+    else:
+        table.title = 'HeadHunter Moscow'
     return table.table
 
-
-def create_table_hh(processed_information):
-    table_data = [
-        ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
-    ]
-    information_keys = list(processed_information.keys())
-    for key in information_keys:
-        data = [key, processed_information[key]['vacancies_found'], processed_information[key]['vacancies_processed'], processed_information[key]['average_salary']]
-        table_data.append(data)
-    table = terminaltables.AsciiTable(table_data)
-    table.title = 'HeadHunter Moscow'
-    return table.table
 
 if __name__ == '__main__':
     load_dotenv()
     token = os.getenv('SJ_SECRET_KEY')
     programming_languages = ['Python', 'Java', 'C++', 'JavaScript']
-    # print(create_table_hh(predict_rub_salary_hh(programming_languages)))
-    print(create_table_sj(predict_rub_salary_sj(token ,programming_languages)))
+    # print(create_table(predict_rub_salary_hh(programming_languages), 'hh'))
+    print(create_table(predict_rub_salary_sj(token, programming_languages), 'sj'))
